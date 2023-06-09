52bc0051c203877e99e1101238223ba0f1d69813
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-06-09 16:02:40 +0800

    8.19 stable - new feature: filter books by genre

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,28 +1,39 @@
+import { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../guery'
 
 const Books = (props) => {
-
   const result = useQuery(ALL_BOOKS)
-  
+  const [selectedGenre, setSelectedGenre] = useState(null)
+
   if (!props.show) {
     return null
   }
-  
+
   if (result.loading) {
     return <div>
       loading...
     </div>
   }
-  
-  const books = result.data.allBooks
-  
-  console.log(books)
 
+  const books = result.data.allBooks
+
+  console.log(books)
+  const genres = [...new Set(books.flatMap(book => book.genres))]
+  console.log(genres)
+  console.log(selectedGenre)
+  const filteredBooks = books.filter(book => book.genres.includes(selectedGenre))
+  console.log(filteredBooks)
   return (
     <div>
       <h2>books</h2>
-
+      <div>
+        <p>in genre patterns</p>
+        {genres.map(genre =>
+          <button onClick={() => setSelectedGenre(genre)}>{genre}</button>
+        )}
+        <button onClick={() => setSelectedGenre(null)}>All genres</button>
+      </div>
       <table>
         <tbody>
           <tr>
@@ -30,16 +41,31 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {!selectedGenre ?
+            <>
+              {books.map((book) => (
+                <tr key={book.title}>
+                  <td>{book.title}</td>
+                  <td>{book.author.name}</td>
+                  <td>{book.published}</td>
+                </tr>
+              ))}
+            </>
+            :
+            <>
+              {filteredBooks.map((book) => (
+                <tr key={book.title}>
+                  <td>{book.title}</td>
+                  <td>{book.author.name}</td>
+                  <td>{book.published}</td>
+                </tr>
+              ))}
+            </>
+          }
+
         </tbody>
       </table>
-    </div>
+    </div >
   )
 }
 
